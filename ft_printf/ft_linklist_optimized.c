c05f1326df5d99cdf91dd0eec7196188478766bc
Gedion1Bekur <Gedion1Bekur@users.noreply.github.com>
2026-06-28 23:16:25 +0200

    feat: refactor linked list implementation and add new functions

---
+++ ft_printf/ft_linklist_optimized.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_linklist_optimized.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gbekur <gbekur@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/28 19:15:20 by gbekur            #+#    #+#             */
+/*   Updated: 2026/06/28 23:16:16 by gbekur           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// #include <stdio.h>
+// #include <stdlib.h>
+
+// struct			node
+// {
+// 	int			data;
+// 	struct node	*link;
+// };
+// // there must be alwa
+
+// int main()
+// {
+
+// 	struct node *head = (struct node *)malloc(sizeof(struct node));
+// 	head->data = 1,
+// 	head->link =NULL;
+
+
+
+// 	struct node *ptr;
+
+// 	ptr = struct add_at_end(head , 
+
+	
+// }
