b0bb9406a22ee56591989d2946b6aae25a57ea80
Gedion1Bekur <Gedion1Bekur@users.noreply.github.com>
2026-07-06 23:07:27 +0200

    feat: add ft_strjoin function to concatenate two strings

---
+++ libft/ft_strjoin.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gbekur <gbekur@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/06 22:32:21 by gbekur            #+#    #+#             */
+/*   Updated: 2026/07/06 23:06:28 by gbekur           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char *buf;
+	size_t i;
+	size_t j;
+
+	i = 0;
+	j = 0;
+	if (!s1 || !s2)
+		return (NULL);
+	buf = (char *)malloc(ft_strlen(s1) + ft_strlen(s2)  + 1);
+	if (!buf)
+		return (NULL);
+	while (s1[i])
+	{
+		buf[i] = s1[i];
+		i++;
+	}
+	while (s2[j])
+	{
+		buf[i] = s2[j];
+		i++;
+		j++;
+	}
+	buf[i] = '\0';
+	return (buf);
+}
