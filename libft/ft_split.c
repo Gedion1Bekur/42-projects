474a993e33b2175761036ee462d7b2c7b8bcc362
Gedion1Bekur <Gedion1Bekur@users.noreply.github.com>
2026-07-08 18:21:28 +0200

    feat: implement ft_split function for string splitting; add helper functions and memory management

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,11 +6,107 @@
 /*   By: gbekur <gbekur@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/07 23:22:40 by gbekur            #+#    #+#             */
-/*   Updated: 2026/07/07 23:22:57 by gbekur           ###   ########.fr       */
+/*   Updated: 2026/07/08 18:21:05 by gbekur           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char **ft_split(char const *s, char c)
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gbekur <gbekur@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/07 23:22:40 by gbekur            #+#    #+#             */
+/*   Updated: 2026/07/08 18:30:15 by gbekur           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static int	ft_count_words(const char *s, char c)
 {
-    
+	int	i;
+	int	words;
+
+	if (!s)
+		return (0);
+	i = 0;
+	words = 0;
+	while (s[i])
+	{
+		if (s[i] != c)
+		{
+			words++;
+			while (s[i] && s[i] != c)
+				i++;
+		}
+		else
+			i++;
+	}
+	return (words);
+}
+
+static char	*word_splitter(const char *s, char c, int *step)
+{
+	char	*word;
+	int		i;
+
+	i = 0;
+	while (s[i] && s[i] != c)
+		i++;
+	*step = i;
+	word = malloc(sizeof(char) * (i + 1));
+	if (!word)
+		return (NULL);
+	i = 0;
+	while (s[i] && s[i] != c)
+	{
+		word[i] = s[i];
+		i++;
+	}
+	word[i] = '\0';
+	return (word);
+}
+
+static char	**ft_free_all(char **words, int j)
+{
+	while (j > 0)
+	{
+		j--;
+		free(words[j]);
+	}
+	free(words);
+	return (NULL);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	int		i;
+	int		j;
+	int		step;
+	char	**words;
+
+	if (!s)
+		return (NULL);
+	words = malloc(sizeof(char *) * (ft_count_words(s, c) + 1));
+	if (!words)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (s[i])
+	{
+		if (s[i] != c)
+		{
+			words[j] = word_splitter(&s[i], c, &step);
+			if (!words[j])
+				return (ft_free_all(words, j));
+			j++;
+			i += step;
+		}
+		else
+			i++;
+	}
+	words[j] = NULL;
+	return (words);
 }
