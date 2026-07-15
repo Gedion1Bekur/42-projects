6f5986b8acddb8b84cad4cce2c45eb95452478e5
Gedion1Bekur <Gedion1Bekur@users.noreply.github.com>
2026-07-15 20:30:59 +0200

    feat: add ft_calloc function and update Makefile; implement ft_lstnew function

--- libft/ft_calloc.c
+++ libft/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: gbekur <gbekur@student.42warsaw.pl>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/06 21:30:01 by gbekur            #+#    #+#             */
-/*   Updated: 2026/07/15 17:17:49 by gbekur           ###   ########.fr       */
+/*   Updated: 2026/07/15 20:30:34 by gbekur           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,13 @@ void	*ft_calloc(size_t count, size_t size)
 	void	*tmp;
 
 	if (count != 0 && size > ((size_t)-1) / count)
-		return (NULL);
+	{
+		tmp = NULL;
+		return (tmp);
+	}
 	tmp = (void *)malloc(count * size);
 	if (!tmp)
 		return (NULL);
-	free(tmp);
 	ft_bzero(tmp, count * size);
 	return (tmp);
 }
--- libft/ft_lstnew.c
+++ libft/ft_lstnew.c
@@ -6,7 +6,20 @@
 /*   By: gbekur <gbekur@student.42warsaw.pl>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/15 18:54:26 by gbekur            #+#    #+#             */
-/*   Updated: 2026/07/15 18:54:27 by gbekur           ###   ########.fr       */
+/*   Updated: 2026/07/15 19:56:33 by gbekur           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*elem;
+
+	elem = (t_list *)malloc(sizeof(t_list));
+	if (!elem)
+		return (NULL);
+	elem->content = content;
+	elem->next = NULL;
+	return (elem);
+}
